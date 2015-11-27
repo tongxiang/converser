2736b5d50e31f9bca55822b13960297ba4e439f6
Tong Xiang <tong.xiang2@gmail.com>
2015-11-27 14:39:37 -0500

    intermed commit, nonworking client

--- routes/message.js
+++ routes/message.js
@@ -1,9 +1,28 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+  , router = express.Router()
+  , Client = require('node-xmpp-client')
+  , config = require('../config.js');
 
 /* GET messages. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.send('respond with a resource');
+
+  var client = new Client({
+    jid: config.botUsername,
+    password: config.botPassword
+  })
+
+  client.on('online', function() {
+    console.log('online')
+    res.send();
+  })
+
+  client.on('stanza', function(stanza) {
+    console.log('Incoming stanza: ', stanza.toString())
+  })
+
+
+
 });
 
 module.exports = router;
